4c585d274c3b82b10a6e451c1cd053bfb178a154
Nabila <nabila.izatin22.12.2000@gmail.com>
2022-12-05 09:03:10 +0700

    commit (16)

--- task_management_app/lib/app/data/controller/auth.controller.dart
+++ task_management_app/lib/app/data/controller/auth.controller.dart
@@ -2,13 +2,14 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:task_management_app/app/routes/app_pages.dart';
 
 class AuthController extends GetxController {
   FirebaseFirestore firestore = FirebaseFirestore.instance;
   UserCredential? _userCredential;
-  final GlobalKey<FormState> formkey = GlobalKey<FormState>();
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
   late TextEditingController searchFriendsController;
 
   @override
@@ -28,10 +29,11 @@ class AuthController extends GetxController {
     searchFriendsController.dispose();
   }
 
-  Future signInWithGoogle() async {
+  Future<void> signInWithGoogle() async {
     // Trigger the authentication flow
     final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
+    print(googleUser!.email);
     // Obtain the auth details from the request
     final GoogleSignInAuthentication? googleAuth =
         await googleUser?.authentication;
@@ -42,32 +44,33 @@ class AuthController extends GetxController {
       idToken: googleAuth?.idToken,
     );
 
-    print(googleUser!.email);
     // Once signed in, return the UserCredential
     await FirebaseAuth.instance
         .signInWithCredential(credential)
         .then((value) => _userCredential = value);
 
-    // Firebase
+// FIREBASE
     CollectionReference users = firestore.collection('users');
 
-    final cekuser = await users.doc(googleUser.email).get();
-    if (cekuser.exists) {
+    final cekUsers = await users.doc(googleUser.email).get();
+
+    if (cekUsers.exists) {
       users.doc(googleUser.email).set({
         'uid': _userCredential!.user!.uid,
         'name': googleUser.displayName,
         'email': googleUser.email,
         'photo': googleUser.photoUrl,
-        'CreatedAt': _userCredential!.user!.metadata.creationTime.toString(),
-        'lastlogin': _userCredential!.user!.metadata.lastSignInTime.toString(),
-        // 'list_cari': (R,RE,REZA)
-      }).then((value) {
+        'createdAt': _userCredential!.user!.metadata.creationTime.toString(),
+        'LastLoginat':
+            _userCredential!.user!.metadata.lastSignInTime.toString(),
+        // 'List_cari':
+      }).then((value) async {
         String temp = '';
         try {
           for (var i = 0; i < googleUser.displayName!.length; i++) {
             temp = temp + googleUser.displayName![i];
-            users.doc(googleUser.email).set({
-              'list_cari': FieldValue.arrayUnion([temp.toUpperCase()])
+            await users.doc(googleUser.email).set({
+              'List_cari': FieldValue.arrayUnion([temp.toUpperCase()])
             }, SetOptions(merge: true));
           }
         } catch (e) {
@@ -75,8 +78,9 @@ class AuthController extends GetxController {
         }
       });
     } else {
-      users.doc(googleUser.email).set({
-        'lastlogout': _userCredential!.user!.metadata.lastSignInTime.toString()
+      users.doc(googleUser.email).update({
+        'LastLoginat':
+            _userCredential!.user!.metadata.lastSignInTime.toString(),
       });
     }
     Get.offAllNamed(Routes.HOME);
@@ -84,17 +88,18 @@ class AuthController extends GetxController {
 
   Future logout() async {
     await FirebaseAuth.instance.signOut();
-    await GoogleSignIn().signOut();
+    GoogleSignIn().signOut();
     Get.offAllNamed(Routes.LOGIN);
   }
 
   var kataCari = [].obs;
-  var hasilPencarian = [].obs;
-  void searchFriend(String keyword) async {
+  var hasilCari = [].obs;
+  void searchFriends(String keyword) async {
     CollectionReference users = firestore.collection('users');
+
     if (keyword.isNotEmpty) {
-      var hasilQuery = await users
-          .where('list_cari', arrayContains: keyword.toUpperCase())
+      final hasilQuery = await users
+          .where('List_cari', arrayContains: keyword.toUpperCase())
           .get();
       if (hasilQuery.docs.isNotEmpty) {
         for (var i = 0; i < hasilQuery.docs.length; i++) {
@@ -103,17 +108,18 @@ class AuthController extends GetxController {
       }
 
       if (kataCari.isNotEmpty) {
+        hasilCari.value = [];
         kataCari.forEach((element) {
           print(element);
-          hasilPencarian.add(element);
+          hasilCari.add(element);
         });
         kataCari.clear();
       }
     } else {
       kataCari.value = [];
-      hasilPencarian.value = [];
+      hasilCari.value = [];
     }
     kataCari.refresh();
-    hasilPencarian.refresh();
+    hasilCari.refresh();
   }
 }
